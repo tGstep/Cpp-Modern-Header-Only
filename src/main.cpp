12308c00ae1405f5bd2a21aa255762ce719b3408
tGstep <38408500+tGstep@users.noreply.github.com>
2025-04-29 01:06:21 +0200

    Update examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
 #include <spdlog/spdlog.h>
 #include <nlohmann/json.hpp>
 
-int main() {
-    spdlog::info("Hello, world!");
-    nlohmann::json j = {{"key", "value"}, {"number", 42}};
-    std::cout << j.dump(2) << std::endl;
-    return 0;
+
+
+int main()
+{
+	nlohmann::json j = {{"key", "value"}, {"number", 42}};
+	spdlog::info("Hello, {}", j.dump());
 }
